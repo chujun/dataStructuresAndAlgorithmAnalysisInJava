7f136334f4cd646b9d1e139fec164f7be0feded5
jun.chu <1571970439@qq.com>
2016-08-06 21:32:55 +0800

    计算最大子序列和的同时输出子序列所有值

--- src/main/java/com/jun/algorithm/first_chapter/SumSubSequenceV4.java
+++ src/main/java/com/jun/algorithm/first_chapter/SumSubSequenceV4.java
@@ -16,6 +16,8 @@ public class SumSubSequenceV4 {
         System.out.println(sumSubSequence(Arrays.asList(-1)));
 
         System.out.println(sumSubSequence(Arrays.asList(2,-3,4,3,-5)));
+
+        System.out.println(sumSubSequenceWithResult(Arrays.asList(1,2,3,-7,3,4,-3,2,4,-11,3,-2)));
     }
 
     /**
@@ -28,12 +30,8 @@ public class SumSubSequenceV4 {
             return 0;
         }
         int maxSum=list.get(0),thisSum=0;
-
-        //用于存储最大子序列值
-        List<Integer> subSequence=new ArrayList<Integer>();
         for (Integer value : list) {
             thisSum += value;
-            subSequence.add(value);
             if (thisSum > maxSum) {
                 //存储当前和最大的子序列和值
                 maxSum = thisSum;
@@ -41,10 +39,47 @@ public class SumSubSequenceV4 {
             if (thisSum < 0) {
                 //重新开始计算可能的最大子序列
                 thisSum = 0;
-                subSequence=new ArrayList<Integer>();
             }
         }
 
+        return maxSum;
+    }
+
+    /**
+     * 改进型:
+     * 同时输出最大子序列
+     * 时间复杂度也是O(N)
+     * @param list
+     * @return
+     */
+    public static int sumSubSequenceWithResult(List<Integer> list){
+        if(null==list||list.size()==0){
+            return 0;
+        }
+        int maxSum=list.get(0),thisSum=0;
+        //用于存储最大子序列值
+        int maxStart=0,maxEnd=0;
+        int tempStart=0;
+        for (int i=0;i<list.size();i++) {
+            thisSum += list.get(i);
+            if (thisSum > maxSum) {
+                //存储当前和最大的子序列和值
+                maxSum = thisSum;
+                if(maxStart!=tempStart){
+                    maxStart=tempStart;
+                }
+                maxEnd=i;
+            }
+            if (thisSum < 0) {
+                //重新开始计算可能的最大子序列
+                thisSum = 0;
+                tempStart=i+1;
+            }
+        }
+        List<Integer> subSequence=new ArrayList<Integer>();
+        for(int i=maxStart;i<=maxEnd;i++){
+            subSequence.add(list.get(i));
+        }
         System.out.println(subSequence);
         return maxSum;
     }
